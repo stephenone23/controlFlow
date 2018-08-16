31a326333e37a6e7a7a682178e3cd1ef3f59ccb8
stephenone23 <stephenwilliamz@yahoo.com>
2018-08-15 19:54:24 -0500

    first commit

---
+++ src/QuestionOne.java
@@ -0,0 +1,13 @@
+public class QuestionOne {
+    public static void main(String[] args) {
+        String name = "Stephen";
+        if (name=="Stephen"){
+            System.out.println("Hello Stephen!");
+        }   else {
+            System.out.println("Invalid entry");
+        }
+        String bell = "Fire!";
+        System.out.println((bell=="Fire!") ? "There is a fire!" : "False alarm");
+        //
+    }
+}
